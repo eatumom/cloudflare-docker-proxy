b5e5483daa25c874b4f2a65fc17f68dac566cc5e
eatumom <80268544+eatumom@users.noreply.github.com>
2024-08-27 15:33:00 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,14 +7,14 @@ const dockerHub = "https://registry-1.docker.io";
 
 const routes = {
   // production
-  "docker.libcuda.so": dockerHub,
-  "quay.libcuda.so": "https://quay.io",
-  "gcr.libcuda.so": "https://gcr.io",
-  "k8s-gcr.libcuda.so": "https://k8s.gcr.io",
-  "k8s.libcuda.so": "https://registry.k8s.io",
-  "ghcr.libcuda.so": "https://ghcr.io",
-  "cloudsmith.libcuda.so": "https://docker.cloudsmith.io",
-  "ecr.libcuda.so": "https://public.ecr.aws",
+  "docker.eatumom.cc": dockerHub,
+  "quay.eatumom.cc": "https://quay.io",
+  "gcr.eatumom.cc": "https://gcr.io",
+  "k8s-gcr.eatumom.cc": "https://k8s.gcr.io",
+  "k8s.eatumom.cc": "https://registry.k8s.io",
+  "ghcr.eatumom.cc": "https://ghcr.io",
+  "cloudsmith.eatumom.cc": "https://docker.cloudsmith.io",
+  "ecr.eatumom.cc": "https://public.ecr.aws",
 
   // staging
   "docker-staging.libcuda.so": dockerHub,
